a858cc229def2184bfa8489356317489df417b6d
Ma2terB1ast3r <jeep@juzzlea.com>
2024-09-27 20:46:43 +1000

    Added fetch to get latest OWASP practices and formatted output

--- scanner.py
+++ scanner.py
@@ -4,33 +4,72 @@ requiredHeaders = {}
 
 URL = ''
 
+def fetchLatestPractices():
+    '''Fetches the latest HTTP header best practices from the OWASP secure headers project. https://owasp.org/www-project-secure-headers/
+    '''
+    OWASP_HEADERS = requests.get('https://owasp.org/www-project-secure-headers/ci/headers_add.json').json()
+
+    headers = {}
+    for key in OWASP_HEADERS['headers']:
+        headers[key['name']] = key['value']
+
+    return headers
+
 def analyseHeaders(requiredHeaders, responseHeaders):
     '''Checks which of the 'requiredHeaders' are in the 'responseHeaders'.
     '''
+    # Init results object
+    results = {
+        'missingHeaders':{
+        },
+        'bestPracticeHeaders':{
+        },
+        'notBestPracticeHeaders':{
+        }
+    }
+
     # Find the headers that are present in the header
     presentHeaders = requiredHeaders.keys() & responseHeaders.keys()
-    print("Present Headers: ")
-    print(presentHeaders)
 
     # Find the headers that aren't present
     missingHeaders = requiredHeaders.keys() - responseHeaders.keys()
-    print("Missing Headers: ")
-    print(missingHeaders)
+    for header in missingHeaders:
+        results['missingHeaders'][header] = requiredHeaders[header]
 
     # Check which of the present headers have the best practice value
     bestPracticeHeaders = {}
     for header in presentHeaders:
         if requiredHeaders[header] == responseHeaders[header]:
-            bestPracticeHeaders[header] = True
+            results['bestPracticeHeaders'][header] = requiredHeaders[header]
         else:
-            bestPracticeHeaders[header] = False
+            results['notBestPracticeHeaders'][header] = requiredHeaders[header]
+
+    return results
+
+def outputResults(results):
+    '''Outputs the results.
+    '''
+    print("Missing Headers: ")
+    print(results['missingHeaders'])
     print("Best Practice Headers: ")
-    print(bestPracticeHeaders)
+    print(results['bestPracticeHeaders'])
+    print("Not Best Practice Headers: ")
+    print(results['notBestPracticeHeaders'])
 
 def main():
-    response = requests.get(URL)
-    analyseHeaders(requiredHeaders, response.headers)
-    # print(response.headers)
+    # Fetch latest best practices
+    requiredHeaders = fetchLatestPractices()
+
+    # Fetch target headers
+    targetHeaders = requests.get(URL).headers
+    # print("Target Headers: ")
+    # print(targetHeaders)
+
+    # Check target headers against best practices
+    results = analyseHeaders(requiredHeaders, targetHeaders)
+
+    # Output results
+    outputResults(results)
 
 if __name__ == "__main__":
     main()
